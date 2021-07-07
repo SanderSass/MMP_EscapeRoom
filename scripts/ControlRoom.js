b2be8a8b0ce10394a2312b17a868c42d3dc5c053
Christof145 <43606559+Christof145@users.noreply.github.com>
2021-07-07 14:42:43 +0200

    fixed image size

--- scripts/ControlRoom.js
+++ scripts/ControlRoom.js
@@ -5,14 +5,17 @@ class ControlRoom extends Phaser.Scene {
 
     preload() {
         this.roomName;
-
         this.load.image('dirArrowLeft', 'assets/directionArrow.png');
+        this.load.image('controlRoom', 'assets/controlRoom.png');
     } //end preload
 
     create() {
         this.roomName = this.add.text(20, 20, 'Control room', { fontSize: '20px', fill: '#FFFFFF' });
         var dirArrowToCommonRoom = this.add.image(50, 100, 'dirArrowLeft').setInteractive();
-
+        var bgControlRoom = this.add.image(0, 0, 'controlRoom').setInteractive();
+        bgControlRoom.setOrigin(0);
+        bgControlRoom.setScale(1.02, 0.93);
+        
         //assets visibility
         dirArrowToCommonRoom.setAlpha(0.2);
 
