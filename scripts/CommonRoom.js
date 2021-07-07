a7fea04d35f09f71b0558a2b859881989dce2f19
Christof145 <43606559+Christof145@users.noreply.github.com>
2021-07-07 12:30:56 +0200

    Added code to common room

--- scripts/CommonRoom.js
+++ scripts/CommonRoom.js
@@ -1,3 +1,7 @@
+var timesClicked = 0;
+var key;
+var value;
+var foundRemoteKey = false;
 class CommonRoom extends Phaser.Scene {
     constructor() {
         super('commonRoom')
@@ -5,18 +9,32 @@ class CommonRoom extends Phaser.Scene {
 
     preload() {
         this.roomName;
+        this.load.image('painting', 'assets/painting.jpg');
+        this.load.image('key', 'assets/key.jpg');
+        this.load.image('remoteBox', 'assets/remoteBox.png');
     } //end preload
 
     create() {
         this.roomName = this.add.text(20, 35, 'Common room', { fontSize: '20px', fill: '#FFFFFF' });
         var engineRoomDoors = this.add.image(100, 500, 'subDoors').setInteractive();
         var controlRoomDoors = this.add.image(700, 500, 'subDoors').setInteractive();
+        var paintingOnWall = this.add.image(300, 300, 'painting').setInteractive();
+        key = this.add.image(100, 100, 'key').setInteractive();
+        var remoteBox = this.add.image(500, 500, 'remoteBox').setInteractive();
+        key.setVisible(false);
 
+        //random number
+        value = Phaser.Math.Between(1, 10);
+            
+        key.setScale(0.2);
+        remoteBox.setScale(0.2);
+        paintingOnWall.setScale(0.2);
         engineRoomDoors.setScale(0.2);
         controlRoomDoors.setScale(0.2);
 
         engineRoomDoors.on('pointerdown', this.onEngineDoorClick, this);
         controlRoomDoors.on('pointerdown', this.onControlDoorClick, this);
+        paintingOnWall.on('pointerdown', this.onPaintingClick, this);
 
     } //end create
 
@@ -27,8 +45,14 @@ class CommonRoom extends Phaser.Scene {
     onControlDoorClick(){
         this.scene.start("controlRoom");
     }
+    onPaintingClick(){
+        timesClicked +=1;
+    }
 
     update() {
-
+        if (timesClicked > value) {
+            key.setVisible(true);
+        }
+        console.log(value);
     } //end update
 } //end title scene
