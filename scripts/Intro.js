6bdd47911cefc2ddd4d30e87aeca5a406af882bf
Jonas_R <jramanauskas120@gmail.com>
2021-07-08 15:44:29 +0200

    intro js fixed

--- scripts/Intro.js
+++ scripts/Intro.js
@@ -1,49 +1,51 @@
-var intro
-var start
+var introVideo
+var startButton
 class Intro extends Phaser.Scene {
     constructor() {
         super('Intro');
     }
-
     preload() {
-        this.roomName;
+
+        this.isVideoPaused = true;
+        this.scale.setGameSize(1920,1080);
+        this.scene.stop("UIScene");
+
+        this.load.video('intro', 'assets/video/mmp_intro.mp4', 'loadeddata', false, true); // 15 seconds
         this.load.image('btnStart', 'assets/btnStart.png');
-        this.scale.setGameSize(2300, 1120);
-        this.load.video('intro', 'assets/video/mmp_intro.mp4', 'loadeddata', false, true);
+
     } //end preload
 
     create() {
-        intro = this.add.video(1150, 600, 'intro');
-        intro.setScale(1.2);
-        intro.play(true);
-        intro.setPaused(true);
-        intro.setLoop(false);
 
-        start = this.add.image(1150, 700, 'btnStart').setInteractive();
+        introVideo = this.add.video(1150, 600, 'intro');
+        startButton = this.add.image(1150, 600, 'btnStart').setInteractive();
+
+        this.isVideoPaused = introVideo.setPaused(this.isVideoPaused);
 
         //assets visibility
-        start.setVisible(true);
+        startButton.setVisible(true);
+
+        //assets depth
+        startButton.setDepth(5);
 
         //assets position
 
         //assets scale
-        start.setScale(2);
+        introVideo.setScale(1.2);
 
         //assets Events
-        //start.on('pointerdown', this.onStartClick(), this);
-
-        start.on('pointerover', function () {
-            start.setAlpha(1);
-            start.on('pointerout', function () {
-                start.setAlpha(0);
-            });
+        startButton.on('pointerdown', this.startVideo, this);
+        startButton.on('pointerdown', function(){
+            startButton.setVisible(false);
         });
+
     } //end create
 
-    // OnClicks
-    onStartClick(){
-        this.scene.start("engineRoom");
-
+    OnClicks
+    startVideo(){
+        this.isVideoPaused = false;
+        introVideo.play(true);
+        introVideo.setLoop(false);
     }
 
     update() {
