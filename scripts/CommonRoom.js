18b399c1503225a37e4cf1bbf8363407a992e085
Jonas_R <jramanauskas120@gmail.com>
2021-07-08 17:08:43 +0200

    Wall Painting Game Finished

--- scripts/CommonRoom.js
+++ scripts/CommonRoom.js
@@ -1,6 +1,7 @@
 var timesClicked = 0;
 var key;
 var value;
+var remotePuzzleSolved = false;
 var foundRemoteKey = false;
 var remoteBox;
 var remote;
@@ -11,14 +12,15 @@ class CommonRoom extends Phaser.Scene {
 
     preload() {
         this.roomName;
+        this.paintingOnWall;
+
         this.load.image('painting', 'assets/painting.jpg');
-        this.load.image('key', 'assets/key.jpg');
+        this.load.image('key', 'assets/key.png');
         this.load.image('remoteBox', 'assets/remoteBox.png');
         this.load.image('remote', 'assets/remote.jpg');
         this.load.image('commonRoom', 'assets/commonRoom.png');
         this.load.image('dirArrowLeft', 'assets/directionArrow.png');
         this.load.image('dirArrowRight', 'assets/directionArrowRight.png');
-
 
     } //end preload
 
@@ -26,45 +28,41 @@ class CommonRoom extends Phaser.Scene {
         this.roomName = this.add.text(20, 20, 'Common room', { fontSize: '20px', fill: '#FFFFFF' });
         this.roomName.setDepth(1);
         var bgCommonRoom = this.add.image(0, 0, 'commonRoom').setInteractive();
-        var engineRoomDoors = this.add.image(100, 500, 'subDoors').setInteractive();
-        var controlRoomDoors = this.add.image(700, 500, 'subDoors').setInteractive();
-        var paintingOnWall = this.add.image(1000, 60, 'painting').setInteractive();
+        this.paintingOnWall = this.add.image(606, 122, 'painting').setInteractive().setRotation(0.15);
         var dirArrowToEngineRoom = this.add.image(50, 100, 'dirArrowLeft').setInteractive();
         var dirArrowToControlRoom = this.add.image(1450, 100, 'dirArrowRight').setInteractive();
         var bookshelfBox = this.add.rectangle(800, 50, 15, 30, 0x6666ff);
         remoteBox = this.add.image(1200, 60, 'remoteBox').setInteractive();
         remote = this.add.image(1200, 60, 'remote').setInteractive();
-        key = this.add.image(1000, 100, 'key').setInteractive();
-        
+        key = this.add.image(170, 198, 'key').setInteractive();
+
 
         bgCommonRoom.setOrigin(0);
-        bgCommonRoom.setScale(1.425, 1.1);
-        key.setScale(0.2, 0.2)
-        key.setVisible(false);
-        remote.setVisible(false).setActive(false);
 
         //assets visibility
         dirArrowToEngineRoom.setAlpha(0.2);
         dirArrowToControlRoom.setAlpha(0.2);
+        key.setVisible(false);
+        remote.setVisible(false).setActive(false);
 
         //assets scale
+        bgCommonRoom.setScale(1.425, 1.1);
         dirArrowToEngineRoom.setScale(0.3);
         dirArrowToControlRoom.setScale(0.3);
+        key.setScale(0.2);
+        remoteBox.setScale(0.2);
+        this.paintingOnWall .setScale(0.11);
 
         //random number
         value = Phaser.Math.Between(1, 10);
-            
-        key.setScale(0.2);
-        remoteBox.setScale(0.2);
-        paintingOnWall.setScale(0.2);
-        engineRoomDoors.setScale(0.2);
-        controlRoomDoors.setScale(0.2);
 
         //assets events
         dirArrowToEngineRoom.on('pointerdown', this.onEngineDoorClick, this);
         dirArrowToControlRoom.on('pointerdown', this.onControlDoorClick, this);
-        paintingOnWall.on('pointerdown', this.onPaintingClick, this);
+        this.paintingOnWall.on('pointerdown', this.wallPaintingRotation, this);
         remoteBox.on('pointerdown', this.onRemoteBoxClick, this);
+        key.on('pointerdown', this.pickUpRemoteKey, this);
+
 
         dirArrowToEngineRoom.on('pointerover',function(){
             dirArrowToEngineRoom.setAlpha(1);
@@ -89,9 +87,6 @@ class CommonRoom extends Phaser.Scene {
     onControlDoorClick(){
         this.scene.start("controlRoom");
     }
-    onPaintingClick(){
-        timesClicked +=1;
-    }
     onRemoteBoxClick(){
         if (foundRemoteKey == true) {
             remoteBox.setVisible(false).setActive(false);
@@ -100,11 +95,24 @@ class CommonRoom extends Phaser.Scene {
             console.log("You need to find the key.")
         }
     }
+    wallPaintingRotation(){
+        timesClicked +=1;
+        if(timesClicked >= value && remotePuzzleSolved == false){
+            remotePuzzleSolved = true;
+            key.setVisible(true).setRotation(1.5);
+            this.tweens.add({
+                targets: this.paintingOnWall , //your image that must spin
+                rotation: -0.010, //rotation value must be radian
+                duration: 500 //duration is in milliseconds
+            });
+        }
+    }
+    pickUpRemoteKey(){
+        foundRemoteKey = true;
+        key.setVisible(false);
+    }
 
     update() {
-        if (timesClicked > value) {
-            key.setVisible(true);
-            foundRemoteKey = true;
-        }
+
     } //end update
 } //end title scene
--- scripts/UIScene.js
+++ scripts/UIScene.js
@@ -4,6 +4,7 @@ class UIScene extends Phaser.Scene{
     }
     preload(){
         this.fuseVertical;
+        this.keyVertical;
         this.engineRoomProgressRecWidth = 0;
         this.engineRoomProgressRec;
         this.counterForValve = 0;
@@ -13,22 +14,27 @@ class UIScene extends Phaser.Scene{
         this.load.image('UIRoomStatus', 'assets/UIRoomStatus.png');
         this.load.image('UIPlayerCards', 'assets/UIPlayerCards.png');
         this.load.image('fuseVertical', 'assets/FuseVertical.png');
+        this.load.image('keyVertical', 'assets/Key.png');
     }
     create(){
         var UIInventory = this.add.image(750, 265, 'UIInventory').setInteractive();
         var UIRoomStatus = this.add.image(1050, 265, 'UIRoomStatus').setInteractive();
         var UIPlayerCards = this.add.image(400, 290, 'UIPlayerCards').setInteractive();
         this.fuseVertical = this.add.image(639, 263, 'fuseVertical').setInteractive();
+        this.keyVertical = this.add.image(673, 263, 'keyVertical').setInteractive();
         this.engineRoomProgressRec = this.add.rectangle(1070, 240, this.engineRoomProgressRecWidth, 10, 0x32FF00);
 
         //assets visibility
         this.fuseVertical.setVisible(false);
+        this.keyVertical.setVisible(false);
+        UIPlayerCards.setVisible(false);
 
         //assets scale
         UIInventory.setScale(0.4);
         UIRoomStatus.setScale(0.5, 0.4);
         UIPlayerCards.setScale(0.5);
         this.fuseVertical.setScale(0.4);
+        this.keyVertical.setScale(0.4);
     }
 
     update(){
@@ -40,6 +46,9 @@ class UIScene extends Phaser.Scene{
         if(isLeverPuzzleSolved === true && this.counterForLevers === 0){
             this.counterForLevers++;
             this.engineRoomProgressRec.width += 23;
+        }
+        if(foundRemoteKey === true ){
+            this.keyVertical.setVisible(true);
         }
     }
 }
