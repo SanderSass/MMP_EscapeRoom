9ca9e23fbd8ec9487739e4f2315e881758afcef4
Jonas_R <jramanauskas120@gmail.com>
2021-07-08 18:57:40 +0200

    Common room updated. Safe opening

--- scripts/gameConfig.js
+++ scripts/gameConfig.js
@@ -19,7 +19,7 @@ let config = {
         parent: "theGame"
     },
     pixelArt: true,
-    scene: [EngineRoom, CommonRoom, ControlRoom, UIScene]
+    scene: [CommonRoom, ControlRoom, UIScene]
 }
 const game = new Phaser.Game(config)
 }) //end load listener
