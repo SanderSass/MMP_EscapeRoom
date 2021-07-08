9e2905b7f2b88b3b650a6b9e7c922b17ec656dcf
Jonas_R <jramanauskas120@gmail.com>
2021-07-08 16:13:47 +0200

    Intro scene update

--- scripts/Intro.js
+++ scripts/Intro.js
@@ -1,5 +1,7 @@
 var introVideo
 var startButton
+var counter = 0;
+var videoStarted = false;
 class Intro extends Phaser.Scene {
     constructor() {
         super('Intro');
@@ -18,7 +20,7 @@ class Intro extends Phaser.Scene {
     create() {
 
         introVideo = this.add.video(1150, 600, 'intro');
-        startButton = this.add.image(1150, 600, 'btnStart').setInteractive();
+        startButton = this.add.image(960, 800, 'btnStart').setInteractive();
 
         this.isVideoPaused = introVideo.setPaused(this.isVideoPaused);
 
@@ -43,12 +45,34 @@ class Intro extends Phaser.Scene {
 
     OnClicks
     startVideo(){
+        this.counterTimer();
         this.isVideoPaused = false;
         introVideo.play(true);
         introVideo.setLoop(false);
+        videoStarted = true;
+    }
+
+    startGame(){
+        this.scene.remove("Intro");
+        this.scene.start("engineRoom");
+        this.scene.start("UIScene");
+        this.scale.setGameSize(1500,310);
+    }
+
+    counterTimer(){
+        counter++;
     }
 
     update() {
+    if(videoStarted === true){
+        counter++
+        if(counter === 1000){
+            videoStarted = false;
+        }
+    }
+    if(counter === 800){
+        this.startGame();
+    }
 
     } //end update
 } //end title scene
